7bae24f710c18ecca636820b457e5ab56d9faaf7
Axel Glöckner <axel.glockner00@gmail.com>
2023-12-19 16:53:56 +0100

    printHeap

--- minHeap/src/VisualizeHeap.c
+++ minHeap/src/VisualizeHeap.c
@@ -1,10 +1,28 @@
+//VisualizeHeap.c
 #include "minHeap.h"
 #include <ncurses.h>
 
 void printHeap(MinHeap *heap)
 {
+    clear();
+    //print heap array
+    for(int i=0; i<heap->size; ++i){
+        mvprintw(2, i*4, "%d", heap->array[i].value);
+    }
 
-
+    //print heap structure lines
+    for(int i=0; i<=heap->size/2 ; ++i){
+        int x = i*4-2;
+        int y = 4;
+        mvprintw(y,x,"/");
+        if(2*i<heap->size){
+            mvprintw(y+1,x-1,"/");
+        }
+        if(2*i+1<heap->size){
+            mvprintw(y+1,x+1, "\\");
+        }
+    }
+    refresh();
 }
 
 int main()
@@ -14,7 +32,7 @@ int main()
     cbreak();  // Disable line buffering
     keypad(stdscr, TRUE); // Enable special key handling    
     MinHeap *heap = createMinHeap(15);
-
+    printHeap(heap);
 
     return 0; 
 
